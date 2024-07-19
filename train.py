366a80cdfae4529294b3377a8cdaf505e74bbf13
Sergio Frayle Pérez <sfrayle@aisuperior.com>
2024-07-19 10:37:13 +0200

    Expose experiment name to trainer

--- train.py
+++ train.py
@@ -6,7 +6,11 @@ from modeling.trainer import Trainer
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--dataset", type=Path, required=True)
+    parser.add_argument("--experiment_name", type=str, default=None)
     args = parser.parse_args()
-    trainer = Trainer(dataset_path=args.dataset.absolute().as_posix())
+    trainer = Trainer(
+        dataset_path=args.dataset.absolute().as_posix(),
+        experiment_name=args.experiment_name,
+    )
     trainer.train()
     trainer.save_best_model()
